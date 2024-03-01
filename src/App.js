1f26251156924f73aecc2f61edf9bd176f1a6cbd
NizamAir <nizamutdinov.airat02@gmail.com>
2024-03-01 20:33:57 +0300

    Урок №4

--- src/App.js
+++ src/App.js
@@ -1,61 +1,70 @@
+import React from "react";
 import Card from "./components/Card";
 import Drawer from "./components/Drawer";
 import Header from "./components/Header";
 
-const arr = [
-  {
-    title: 'Мужские Кроссовки Nike Blazer Mid Suede',
-    price: 12999,
-    imageUrl: '/img/sneakers/1.jpg'
-  },
-  {
-    title: 'Мужские Кроссовки Nike Air Max 270',
-    price: 15600,
-    imageUrl: '/img/sneakers/2.jpg'
-  },
-  {
-    title: 'Мужские Кроссовки Nike Blazer Mid Suede',
-    price: 8499,
-    imageUrl: '/img/sneakers/3.jpg'
-  },
-  {
-    title: 'Кроссовки Puma X Aka Boku Future Rider',
-    price: 8999,
-    imageUrl: '/img/sneakers/4.jpg'
-  },
-  
-
-];
 
 function App() {
+
+  const [items, setItems] = React.useState([]);
+
+  const [cartItems, setCartItems] = React.useState([]);
+
+  const [cartOpened, setCartOpened] = React.useState(false);
+
+  React.useEffect(() => {
+    fetch('https://65e1fecda8583365b317c42e.mockapi.io/items')
+      .then(
+        (res) => {
+          return res.json()
+        }
+      )
+      .then(
+        (json) => {
+          setItems(json)
+        });
+  },[]);
+
+  const onAddToCart=(obj)=>{
+    setCartItems(prev => [...prev, obj]);
+  }
+
+
+
   return (
     <div className="wrapper clear">
 
-      <Drawer />
+      {cartOpened && <Drawer items={cartItems} onClose={() => setCartOpened(false)} />}
 
-      <Header />
+
+      <Header
+        onClickCart={() => setCartOpened(true)}
+
+      />
+
       <div className="content p-40">
         <div className="d-flex align-center justify-between mb-40">
           <h1>Все кроссовки</h1>
           <div className="search-block d-flex">
             <img src="/img/search.svg" alt="Search" />
             <input placeholder="Поиск ... " />
-
           </div>
         </div>
+
         <div className="d-flex flex-wrap">
-          {arr.map((obj) => (
+          {items.map((item) => (
             <Card
-              title={obj.title}
-              price={obj.price}
-              imageUrl={obj.imageUrl}
+              title={item.title}
+              price={item.price}
+              imageUrl={item.imageUrl}
+              onPlus={(obj)=>onAddToCart(obj)}
+              onFavorite={() => console.log('Добавили в закладки')}
             />
           ))}
-
-
-
         </div>
+
       </div>
+
     </div>
   );
 }
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,27 +1,33 @@
+import React from 'react';
 import styles from './Card.module.scss';
 
 
-function Card(props) {
+function Card({ title, price, imageUrl,onFavorite,onPlus }) {
 
-	const onClickButton=()=>{
-		alert(1223);
-	};
+	const [isAdded, setIsAdded] = React.useState(false);
+
+	const onClickPlus = () => {
+		onPlus({title, price, imageUrl})
+		setIsAdded(!isAdded);
+	}
+
+	React.useEffect(() => {
+		console.log('Переменная изменилась')
+	}, [isAdded])
 
 	return (
 		<div className={styles.card}>
 			<div className={styles.favorite}>
-				<img src="/img/heart-unliked.svg" alt="Unliked" />
+				<img src="/img/heart-unliked.svg" alt="Unliked" onClick={onFavorite} />
 			</div>
-			<img width={133} height={112} src={props.imageUrl} alt="Sneakers" />
-			<h5>{props.title}</h5>
+			<img width={133} height={112} src={imageUrl} alt="Sneakers" />
+			<h5>{title}</h5>
 			<div className="d-flex justify-between align-center">
 				<div className="d-flex flex-column">
 					<span>Цена:</span>
-					<b>{props.price} руб.</b>
+					<b>{price} руб.</b>
 				</div>
-				<button className="button" onClick={onClickButton}>
-					<img width={11} height={11} src="/img/plus.svg" alt="Plus" />
-				</button>
+				<img className={styles.plus} onClick={onClickPlus} src={isAdded ? "/img/btn-checked.svg" : "/img/btn-plus.svg"} alt="Plus" />
 			</div>
 		</div>
 	);
